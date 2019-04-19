0049af11951e0deb85fe6af7737206083072f676
Karen Gil <albanis21@gmail.com>
2019-04-18 21:37:19 -0700

    Base responsive del sitio y navbar responsive

---
+++ index.js
@@ -0,0 +1,7 @@
+const resNavBar = document.querySelector('i');
+
+resNavBar.addEventListener('click', () => {
+    let mobileMenu = document.querySelector('.main-menu');
+    mobileMenu.classList.toggle('mobile-menu')
+
+})
